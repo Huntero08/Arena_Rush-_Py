aa82304c9ee2ef675593baf1e00e4131fc635b3c
Lana_Vicentini <163560300+Huntero08@users.noreply.github.com>
2025-08-09 18:48:01 -0300

    Add files via upload

---
+++ logger.py
@@ -0,0 +1,14 @@
+import logging
+from datetime import datetime
+
+log_filename = f"jogo_log_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
+
+logging.basicConfig(
+    filename=log_filename,
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%H:%M:%S"
+)
+
+logger = logging.getLogger("game")
+
